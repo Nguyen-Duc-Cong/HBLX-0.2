a495e434e185c0eb526af122814aa2a5fa37a569
hieuntse <hieusedn@gmail.com>
2021-11-11 15:23:56 +0700

    main activity screen

--- app/src/main/java/com/midterm/hblx_02/View/Activity/MainActivity.java
+++ app/src/main/java/com/midterm/hblx_02/View/Activity/MainActivity.java
@@ -1,16 +1,89 @@
 package com.midterm.hblx_02.View.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
 
 import android.os.Bundle;
+import android.os.Handler;
 
+import com.midterm.hblx_02.Model.Photo;
 import com.midterm.hblx_02.R;
+import com.midterm.hblx_02.ViewModel.ZoomOutPageTransformer;
+import com.midterm.hblx_02.ViewModel.photoViewPagerAdapter;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager2 mViewPager2;
+    private List<Photo> mListPhoto;
+    private CircleIndicator3 mCircleIndicator3;
+
+    private Handler mHandler = new Handler();
+    private Runnable mRunable = new Runnable() {
+        @Override
+        public void run() {
+            if(mViewPager2.getCurrentItem() == mListPhoto.size() -1){
+                mViewPager2.setCurrentItem(0);
+            }
+            else{
+                mViewPager2.setCurrentItem(mViewPager2.getCurrentItem() + 1);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager2 = findViewById(R.id.view_pager_2);
+        mCircleIndicator3 = findViewById(R.id.cicrle_indicator);
+
+        mListPhoto = getListPhoto();
+        photoViewPagerAdapter adapter = new photoViewPagerAdapter(mListPhoto);
+        mViewPager2.setAdapter(adapter);
+
+        mCircleIndicator3.setViewPager(mViewPager2);
+
+        mViewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mHandler.removeCallbacks(mRunable);
+                mHandler.postDelayed(mRunable, 3000);
+            }
+        });
+
+        mViewPager2.setPageTransformer(new ZoomOutPageTransformer());
+
+    }
+
+
+
+    private List<Photo> getListPhoto(){
+        List<Photo> list = new ArrayList<>();
+        list.add(new Photo(R.drawable.image1));
+        list.add(new Photo(R.drawable.image2));
+        list.add(new Photo(R.drawable.image3));
+        return list;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHandler.removeCallbacks(mRunable);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.postDelayed(mRunable, 3000);
     }
 }
---
+++ app/src/main/java/com/midterm/hblx_02/ViewModel/photoViewPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.midterm.hblx_02.ViewModel;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.midterm.hblx_02.Model.Photo;
+import com.midterm.hblx_02.R;
+
+import java.util.List;
+
+public class photoViewPagerAdapter extends RecyclerView.Adapter<photoViewPagerAdapter.PhotoViewHolder>{
+
+    private List<Photo> mListPhoto;
+
+    public photoViewPagerAdapter(List<Photo> mListPhoto) {
+        this.mListPhoto = mListPhoto;
+    }
+
+
+    @NonNull
+    @Override
+    public PhotoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_photo, parent,false);
+
+        return new PhotoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PhotoViewHolder holder, int position) {
+        Photo photo = mListPhoto.get(position);
+        if(photo == null){
+            return;
+        }
+        holder.imgPhoto.setImageResource(photo.getResourceID());
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mListPhoto!= null){
+            return mListPhoto.size();
+        }
+        return 0;
+    }
+
+    public class PhotoViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imgPhoto;
+
+
+        public PhotoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_photo);
+        }
+    }
+}
