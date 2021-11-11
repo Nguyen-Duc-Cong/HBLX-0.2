a77346d73d3ef804863c7769e9ac96f1ec67d478
hieuntse <hieusedn@gmail.com>
2021-11-11 23:55:16 +0700

    main activity screen

--- app/src/main/java/com/midterm/hblx_02/Model/Question.java
+++ app/src/main/java/com/midterm/hblx_02/Model/Question.java
@@ -4,19 +4,29 @@ import java.util.ArrayList;
 
 public class Question {
     private String id;
+    private String description;
     private ArrayList<String> answer;
     private ArrayList<String> result;
     private String pathImage;
     private ArrayList<String> userResult;
 
-    public Question() {
+    public Question(String des, ArrayList<String> ans, ArrayList<Integer> rlt, String path) {
     }
 
-    public Question(ArrayList<String> answer, ArrayList<String> result, String pathImage, ArrayList<String> userResult) {
+    public Question(String description, ArrayList<String> answer, ArrayList<String> result, String pathImage, ArrayList<String> userResult) {
+        this.description = description;
         this.answer = answer;
         this.result = result;
         this.pathImage = pathImage;
         this.userResult = userResult;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public ArrayList<String> getAnswer() {
